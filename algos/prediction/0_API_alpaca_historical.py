01a6a6ca38c8dbfbbfd6d7cf1267c1787501d8a1
owen <35806309+owenAPI@users.noreply.github.com>
2023-11-10 05:55:10 -0500

    EDITS

--- algos/prediction/0_API_alpaca_historical.py
+++ algos/prediction/0_API_alpaca_historical.py
@@ -1,9 +1,10 @@
 import requests
 import pandas as pd
 import datetime as dt
+from datetime import timedelta
+from colorama import Back, Style, Fore
 import time
 import json
-
 import _KEYS_DICT
 
 # Alpaca endpoint URL  
@@ -19,7 +20,9 @@ headers = {
     'APCA-API-SECRET-KEY': API_SECRET,
 }
 
-def get_bars(symbol, start_date, end_date, timeframe='5Min', limit=10000):
+user_chosen_interval = input("Please enter your desired interval (1Min, 5Min, 30Min, 1Hour, etc.) : ")
+
+def get_bars(symbol, start_date, end_date, timeframe=user_chosen_interval, limit=10000):
     df_list = []  # List to hold batches of dataframes
     page_token = None  # Page token for pagination
 
@@ -38,20 +41,21 @@ def get_bars(symbol, start_date, end_date, timeframe='5Min', limit=10000):
         # print(f"{base_url}{symbol}/bars",params )
         response = requests.get(f"{base_url}{symbol}/bars", headers=headers, params=params)
 
-        # Check the response
+        # Check the response for OK code
         if response.status_code != 200:
-            print(f"Failed response URL {symbol}: {response.url}")
-            print(f"Failed response to fetch data for {symbol}: {response.content}  ErrorCode: : {response.status_code} ")
-            print(f"Dict response hearders keys {dict(response.headers) }")
+            print(f"{Fore.RED}\nFAILED RESPONSE {symbol}: {response.url}\n")
+            print(f"{Fore.RED}Failed to fetch data for {symbol}: {response.content}  ErrorCode: : {response.status_code}\n")
+            print(f"{Fore.YELLOW}DICT response headers {dict(response.headers)}\n")
+            print(Fore.RESET)
             break
 
         data = response.json()
 
         if not data['bars']:
-            print(f"No data found for {symbol} in the given date range.")
+            print(f"{Fore.RED}No data found for {symbol} in the given date range.")
             break
 
-        # Create a DataFrame and only keep 'Date', 'Open', 'High', 'Low', 'Close', 'Volume'
+        # Create a DataFrame only keeping 'Date', 'Open', 'High', 'Low', 'Close', 'Volume'
         df = pd.DataFrame(data['bars'])
         df = df[['t', 'o', 'h', 'l', 'c', 'v']]
 
@@ -65,49 +69,43 @@ def get_bars(symbol, start_date, end_date, timeframe='5Min', limit=10000):
         # Append the DataFrame to the list
         df_list.append(df)
 
-        # If a next page token is present, continue fetching data, else break
+        # If a next_page_token is present, continue fetching data, else break
         if 'next_page_token' in data and data['next_page_token'] is not None:
-            # print(f"Fetching next page with token {data['next_page_token']}")
             page_token = data['next_page_token']
         else:
-            print("No more pages to fetch.")
+            print(Fore.YELLOW + "\nNo more pages to fetch.")
             break
 
-    # Concatenate all the dataframes in the list
+    # Concatenate all the DataFrames in the list
     final_df = pd.concat(df_list)
 
     # Return the final DataFrame
     return final_df
 
-
-
 # Set your parameters
 # symbol = 'AAPL'  # Replace with the symbol you're interested in
-START_DATE = '2019-01-01T00:00:00Z'
-END_DATE = '2023-11-01T23:59:59Z'
+START_DATE = '2017-01-01T00:00:00Z'
+END_DATE = dt.datetime.utcnow().date().today() - dt.timedelta(days=1)
+
+# END_DATE = '2023-11-01T23:59:59Z'
 CSV_NAME = "@CHILL"
 stocks_list = _KEYS_DICT.DICT_COMPANYS[CSV_NAME]
 
 for symbol in stocks_list:
-    # Fetch the data
-    print("Starting data fetching process Stock: ", symbol)
+    # Fetch data
+    print(Fore.GREEN + "\nStarting data fetching process... \nStock: ", symbol)
     df = get_bars(symbol, START_DATE, END_DATE)
-    print("Data fetching process completed df.shape: ", df.shape)
-
-    # Save the data as a CSV file
+    print(Fore.GREEN + "\nData fetching process completed... \ndf.shape: ", df.shape)
+    
+    # Save data as a CSV file
     if df is not None:
-        #df.index = pd.to_datetime(df.index)#Get TypeError: Index must be DatetimeIndex
-        TIME_ALPHA_OPEN = "13:30:00";TIME_ALPHA_CLOSE = "20:00:00";
+        TIME_ALPHA_OPEN = "13:00:00";TIME_ALPHA_CLOSE = "22:00:00";
         df = df.between_time(TIME_ALPHA_OPEN, TIME_ALPHA_CLOSE)
-        df['Date'] = df.index  #.to_pydatetime()
-        # df.to_csv(f"data/alpa_{symbol}_1min.csv",sep="\t")
-        # print(f"Data saved as ", f"data/alpa_{symbol}_1min.csv")
-        max_recent_date = df.index.max().strftime("%Y%m%d")   # pd.to_datetime().strftime("%Y%m%d")
+        df['Date'] = df.index
+        max_recent_date = df.index.max().strftime("%Y%m%d")
         min_recent_date = df.index.min().strftime("%Y%m%d")
-        print("d_price/alpaca/alpaca_" + symbol + '_' + '5Min' + "_" + max_recent_date + "__" + min_recent_date + ".csv")
-        # df.to_csv("d_price/alpaca/alpaca_" + symbol + '_' + '5Min' + "_" + max_recent_date + "__" + min_recent_date + ".csv",sep="\t", index=None)
-        df.to_csv("d_price/alpaca/alpaca_" + symbol + '_' + '5Min' + "_.csv",sep="\t", index=None)
-        print("\tSTART: ", str(df.index.min()),  "  END: ", str(df.index.max()) , " shape: ", df.shape, "\n")
+        print(Fore.CYAN + "\nd_price/alpaca/alpaca_" + symbol + '_' + user_chosen_interval + "_" + max_recent_date + "__" + min_recent_date + ".csv" + Fore.RESET)
+        df.to_csv("d_price/alpaca/alpaca_" + symbol + '_' + user_chosen_interval + "_.csv",sep="\t", index=None)
+        print(Fore.MAGENTA + "START: ", str(df.index.min()),  "\nEND: ", str(df.index.max()) , "\nSHAPE: ", df.shape, "\n")
     else:
-        print ("error none in stock: ", symbol)
-
+        print(Fore.RED + "ERROR! None in Stock: ", symbol)
